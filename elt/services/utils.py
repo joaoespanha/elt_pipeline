80cd88181bf68ee52ba9fdcbf99fcfa0f7763a7e
joao espanha <jpbesr@gmail.com>
2024-08-22 16:48:05 -0300

    refactor: refactoring check_for_files to return a list of paths and deleting move_csv_file

--- elt/services/utils.py
+++ elt/services/utils.py
@@ -1,28 +1,8 @@
 import os
 import logging
-import shutil
 
 
-def move_csv_file(csv_file, destination):
-    try:
-        # Add 'ex' to the start of the file name
-        base_name = os.path.basename(csv_file)
-        new_name = "x" + base_name
-        new_path = os.path.join(destination, new_name)
-
-        logging.info(f"Renaming file from {base_name} to {new_name}")
-
-        # Move the file to the new directory with the new name
-        shutil.move(csv_file, new_path)
-
-        logging.info(f"CSV file successfully moved to {new_path}")
-
-    except Exception as e:
-        logging.error(f"An error occurred while moving the file: {e}")
-        raise  # Re-raise the exception to handle it later if necessar
-
-
-# Function to check if there are any files in the directory and return the first file path
+# Function to check if there are any files in the directory and return the list of file paths
 def check_for_files(directory_path):
     # Check if the directory exists
     if not os.path.exists(directory_path):
@@ -30,12 +10,16 @@ def check_for_files(directory_path):
         return None
 
     # Iterate through the items in the directory
+    files = []
     for item in os.listdir(directory_path):
         # Create the full path
         item_path = os.path.join(directory_path, item)
         # Check if the item is a file
         if os.path.isfile(item_path):
-            return item_path  # Return the first file path found
+            files.append(item_path)
 
-    logging.info(f"No files found in directory {directory_path}.")
-    return None
+    if len(files) == 0:
+        logging.info(f"No files found in directory {directory_path}.")
+        return None
+
+    return files
