ca6cdf394864b34cfd5134c25000db58e4bf6791
joao espanha <jpbesr@gmail.com>
2024-08-26 12:21:28 -0300

    refactor: changing main.py elt logic to eliminate postgress extraction phase for memory efficiency

--- elt/main.py
+++ elt/main.py
@@ -1,6 +1,6 @@
+import logging
 from model.pg_connections.dev_main import process_parquet_to_postgres
 from services.utils import check_for_files
-from model.pg_connections.dev_main import get_full_pg_data
 from services.transformations.main import transform_data
 from model.google_connections.bigquery import load_dataframes_to_bigquery
 from services.get_order_proof_data import get_order_proof_data
@@ -10,13 +10,25 @@ destination_dir = (
     "elt/data/processed_parquets"  # Destination directory for processed files
 )
 table_name = "raw_parquet_orders"  # Replace with your desired table name
-batch_size = 10000  # Define the batch size for loading
+batch_size = 25000  # Define the batch size for loading
 log_file_path = "elt/parquet_to_pg/parquet_process.log"  # Log file path
 
+log_file_path = "elt/main.log"
+
+# Configuração do logging
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s - %(levelname)s - %(message)s",
+    handlers=[
+        logging.FileHandler(log_file_path),  # Logging em arquivo
+        logging.StreamHandler(),  # Logging no console (terminal)
+    ],
+)
 
 # Record the start time of the script
 # Step 1: Check if there are files in the directory and get the first file path
 parquet_file_paths = check_for_files(directory_path)
+logging.info(f"Parquet file paths: {parquet_file_paths}")
 
 order_proof_data = get_order_proof_data(
     "desafio-eng-dados", prefix="evidencias_atendimentos/"
@@ -25,20 +37,21 @@ order_proof_data = get_order_proof_data(
 load_dataframes_to_bigquery(order_proof_data)
 
 for parquet_file_path in parquet_file_paths:
+    logging.info(f"Running for {parquet_file_path}")
+
     # Step 2: Process the parquet file and load it into PostgreSQL
-    process_parquet_to_postgres(parquet_file_path, table_name, batch_size)
+    new_data_df = process_parquet_to_postgres(parquet_file_path, table_name, batch_size)
 
-    print("parquet processing completed successfully.")
+    if not new_data_df.empty:
+        logging.info("parquet processing completed successfully.")
 
-    print("Starting data transformation...")
-    pg_data = get_full_pg_data()
+        load_data = transform_data(new_data_df)
 
-    load_data = transform_data(pg_data)
+        logging.info("Data transformation completed successfully.")
 
-    print("Data transformation completed successfully.")
-    print(transform_data)
-
-    # Step 3: Load the transformed data into BigQuery
-    print("Starting data loading to BigQuery...")
-    load_dataframes_to_bigquery(load_data)
-    print("Data loading to BigQuery completed successfully.")
+        # Step 3: Load the transformed data into BigQuery
+        logging.info("Starting data loading to BigQuery...")
+        load_dataframes_to_bigquery(load_data, batch_size)
+        logging.info("Data loading to BigQuery completed successfully.")
+    else:
+        logging.error("parquet processing skipped.")
